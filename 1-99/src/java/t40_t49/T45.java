3614ba380c5630931f2020fc27a25652e960265c
Lxp_javaer <490408824@qq.com>
2020-05-05 16:01:31 +0800

    2020/05/05

---
+++ 1-99/src/java/t40_t49/T45.java
@@ -0,0 +1,31 @@
+package java.t40_t49;
+
+public class T45 {
+    class Solution {
+        public int jump(int[] nums) {
+            //典型的贪心算法题目，要求最短的步数
+            //换个角度看，可以理解为怎么使每步跳的最远
+            //注意，这里是指，平均每步跳的最远
+            //res记录结果，boundary记录每步能达到的边界，maxStep最大步长
+            int res = 0, boundary = 0, maxStep = 0;
+            //从头到尾按序遍历
+            for (int i = 0; i < nums.length - 1; i++) {
+                //找到能到达的格子里，能跳最远的格子
+                //每个格子能达到的最远距离，等于自身数值加自身索引
+                //例如示例1[2,3,1,1,4],nums[0]=2所能达到的格子只有两个
+                //nums[1]=3+1=4,nums[2]=1+2=3;
+                //那么无论再往后如何，第一步最优解一定是跳到nums[1]，因为平均起来步长最大
+                maxStep = Math.max(maxStep, nums[i] + i);
+                //如果达到边界，即当前格子所能达到的所有格子都遍历完了
+                if (i == boundary) {
+                    //更新边界
+                    boundary = maxStep;
+                    //步长+1
+                    res++;
+                }
+            }
+            //返回结果
+            return res;
+        }
+    }
+}
---
+++ 1-99/src/java/t40_t49/T46.java
@@ -0,0 +1,49 @@
+package java.t40_t49;
+
+public class T46 {
+    class Solution {
+        //提取为全局变量，方便调用
+        //首先根据返回类型创建结果集
+        List<List<Integer>> res = new ArrayList<>();
+        //创建临时列表，存放单词遍历的结果
+        List<Integer> list = new ArrayList<>();
+
+        public List<List<Integer>> permute(int[] nums) {
+            //创建标志数组，表示某个数是否已经遍历过
+            boolean[] visited = new boolean[nums.length];
+            //典型的回溯算法题，这题可采取dfs/bfs，这里主要讲解dfs
+            dfs(nums, 0, visited);
+            //返回结果
+            return res;
+        }
+
+        //编写dfs算法
+        //nums需要遍历的数组，len已遍历的长度，visited标志列表
+        public void dfs(int[] nums, int len, boolean[] visited) {
+            //dfs/bfs/回溯等递归调用的方法，第一步，优先编写退出条件
+            //即列表长度达到数组长度，即单次遍历结束
+            if (len == nums.length) {
+                //将结果放入结果集
+                res.add(new ArrayList<>(list));
+                //退出遍历
+                return;
+            }
+            //循环遍历每一个值
+            for (int i = 0; i < nums.length; i++) {
+                //如果该数没有遍历过，则可以使用
+                if (!visited[i]) {
+                    //更新标志位
+                    visited[i] = true;
+                    //将该数放入列表
+                    list.add(nums[i]);
+                    //往下继续遍历
+                    dfs(nums, len + 1, visited);
+                    //重点！当回溯时，一定要删除之前添加的值
+                    list.remove(list.size() - 1);
+                    //同时要把标志位重置
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
